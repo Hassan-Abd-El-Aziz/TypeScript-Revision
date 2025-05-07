97fbc61d3eee31cc3579a55e07dedbb0118be044
Hassan-Abd-El-Aziz <hassanabdelaziz940@gmail.com>
2025-05-07 21:38:09 +0300

    Add-some lessons

--- dist/index.js
+++ dist/index.js
@@ -20,8 +20,27 @@ function sum(num1, num2) {
     return `Total num is ${total}`;
 }
 console.log(sum(10, 20));
-function Employees(username = "NoName", salary, userage) {
+let sums = function (num1, num2) {
+    return num1 + num2;
+};
+console.log(sums(10, 20));
+let arow = (num1, num2) => num1 + num2;
+console.log(arow(20, 30));
+function employees(username = "NoName", salary, userage) {
     return `Heloo ${username} Your age is ${userage} your salary is ${salary}`;
 }
-console.log(Employees("Ahmed", 35000, 35));
+console.log(employees("Ahmed", 35000, 35));
+function nums(...num) {
+    let result = 0;
+    for (let i = 0; i < num.length; i++) {
+        result += num[i];
+    }
+    return `restult is ${result}`;
+}
+console.log(nums(10, 20, 30, 40));
+let uname = "Hassan";
+function Page(btn) {
+    return ` btn up is ${btn.up} - ${btn.left} - ${btn.right}`;
+}
+console.log(Page({ right: "GoRight", left: "GoLeft", up: true }));
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -22,14 +22,49 @@ function sum(num1: number, num2: number): string {
 }
 console.log(sum(10, 20));
 
+//anonymous Function
+let sums = function (num1: number, num2: number): number {
+  return num1 + num2;
+};
+console.log(sums(10, 20));
+//Arow func
+let arow = (num1: number, num2: number): number => num1 + num2;
+console.log(arow(20, 30));
+
 //optional & defult parameter
 
-function Employees(
+function employees(
   username: string = "NoName",
   salary: number,
   userage?: number
 ): string {
   return `Heloo ${username} Your age is ${userage} your salary is ${salary}`;
 }
-console.log(Employees("Ahmed", 35000, 35));
+console.log(employees("Ahmed", 35000, 35));
 
+//rest Parameter
+function nums(...num: number[]): string {
+  let result: number = 0;
+  for (let i = 0; i < num.length; i++) {
+    result += num[i];
+  }
+  // num.forEach((val) => {
+  //   result += val;
+  // });
+  return `restult is ${result}`;
+}
+
+console.log(nums(10, 20, 30, 40));
+
+//Type Alias
+type st = string;
+let uname: st = "Hassan";
+type Buttons = {
+  right: string;
+  left: string;
+};
+type mix = Buttons & { up: boolean };
+function Page(btn: mix) {
+  return ` btn up is ${btn.up} - ${btn.left} - ${btn.right}`;
+}
+console.log(Page({ right: "GoRight", left: "GoLeft", up: true }));
